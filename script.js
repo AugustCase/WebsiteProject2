77f8e7ba7f8cdbf4ee6b29e1016997f0d19526d1
August <ACasebca@gmail.com>
2025-04-06 13:00:08 -0400

    added a for loop

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ function sendApiRequest() {
     console.log(userInput)
 
     var giphyApiKey = 'vZ6ZX9AJ6RDnvG1HxdeImauUtVS0wjfr'
-    var giphyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=r&api_key=${giphyApiKey}&limit=30'
+    var giphyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=r&api_key=${giphyApiKey}&limit=30&offset=0&lang=en'
     var giphyApiURL = giphyApiUrl.replace('${userInput}', userInput).replace('${giphyApiKey}', giphyApiKey)
 
     fetch(giphyApiURL).then(function(data) {
         return data.json()
     })
-    .then(function(json){
-        console.log(json.data[0].images.fixed_height.url)
-        var imgPath = json.data[0].images.fixed_height.url
+
+.then(function(json) {
+    for (var i = 0; i < json.data.length; i++) {
+        console.log(json.data[i])
+        var imgPath = json.data[i].images.fixed_height.url
         var img = document.createElement("img")
         img.setAttribute("src", imgPath)
         document.body.appendChild(img)
+        document.querySelector("#input").value = ""
+    }
+
     })
 }
